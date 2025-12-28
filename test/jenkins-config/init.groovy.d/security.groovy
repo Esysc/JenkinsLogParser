0eb3b55c2f86f5a03f414e1024ccc027c1d70ce7
ACS <Esysc@users.noreply.github.com>
2025-12-28 09:24:37 +0100

    fix Jenkins job volume

--- test/jenkins-config/init.groovy.d/security.groovy
+++ test/jenkins-config/init.groovy.d/security.groovy
@@ -13,6 +13,26 @@ def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 strategy.setAllowAnonymousRead(true)
 instance.setAuthorizationStrategy(strategy)
 
+// Copy job configurations from mounted directory
+def jobsSource = new File("/tmp/jenkins-jobs")
+def jobsTarget = new File("/var/jenkins_home/jobs")
+
+if (jobsSource.exists() && jobsSource.isDirectory()) {
+    jobsSource.eachDir { jobDir ->
+        def targetJobDir = new File(jobsTarget, jobDir.name)
+        if (!targetJobDir.exists()) {
+            targetJobDir.mkdirs()
+            def configFile = new File(jobDir, "config.xml")
+            if (configFile.exists()) {
+                def targetConfig = new File(targetJobDir, "config.xml")
+                targetConfig.text = configFile.text
+                println "Created job: ${jobDir.name}"
+            }
+        }
+    }
+}
+
 instance.save()
+instance.reload()
 
 println "Jenkins security configured - user: admin / password: admin"
